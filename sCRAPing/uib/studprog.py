62bd19bc773b579c5b518f319ae8b0bd5d39776e
wittrup <wittrup@gmail.com>
2017-02-01 23:02:36 +0100

    Find fucking fault tomorrow

--- sCRAPing/uib/studprog.py
+++ sCRAPing/uib/studprog.py
@@ -3,24 +3,54 @@ from os.path import isfile, exists
 import os
 import pickle
 import studprogparser
+from urllib.parse import urlparse
 
-
-url = r'http://www.uib.no/studieprogram'
 
 def mkdir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-mkdir(r'ignore')
-if isfile(r'ignore/studieprogram'):
-    r = pickle.load(open(r'ignore/studieprogram', 'rb'))
-else:
-    r = requests.get(url)
-    pickle.dump(r, open(r'ignore/studieprogram', 'wb'), pickle.HIGHEST_PROTOCOL)
+def loadorget(url, filename, reload=False):
+    if reload or not isfile(filename):
+        r = requests.get(url)
+        pickle.dump(r, open(filename, 'wb'), pickle.HIGHEST_PROTOCOL)
+    else:
+        r = pickle.load(open(filename, 'rb'))
+    return r
 
-parser = studprogparser.StudProgParser()
-parser.feed(r.text)
+def getstudpage(url, fil):
+    r = loadorget(url, fil)
+    studpageparser = studprogparser.StudPageParser()
+    studpageparser.feed(r.text)
+    return studpageparser
 
-for cat, list in parser.result.items():
-    for items in list:
-        print(cat, *items, sep='\t')
+if __name__ == '__main__':
+    url = r'http://www.uib.no/studieprogram'
+    mkdir(r'ignore')
+    r = loadorget(url, r'ignore/studieprogram')
+
+    parser = studprogparser.StudProgParser()
+    parser.feed(r.text)
+
+    n = 0
+    out =open(r'output.tsv', 'w', encoding='UTF-8')
+    print('cat', 'studie', 'url', 'studieplasser', 'poenggrenser', 'obligatorisk emner', sep='\t', file=out)
+    for cat, list in parser.result.items():
+        for items in list:
+            url = r'http://www.uib.no' + items[-1]
+            s = getstudpage(url, r'ignore/' + urlparse(url).path.split('/')[-1])
+            h = s.heading()
+
+            subject = s.data('subject')
+            studata = s.data('uib-study-data')
+            d = {'obligatorisk emne': []}
+            if studata:
+                for k, v in studata:
+                    i = subject[k]
+                    d[i] = d[i] + [v] if i in d else [v]
+
+            print(cat, *items, h['studieplassar'], h['poenggrense'], d['obligatorisk emne'], sep='\t', file=out)
+
+            if n > 10:
+                break
+            n += 1
--- sCRAPing/uib/studprogparser.py
+++ sCRAPing/uib/studprogparser.py
@@ -1,4 +1,8 @@
 from html.parser import HTMLParser
+import re
+
+
+PATTERN_uib_study_data = r'([A-Za-z]+\d+)|(\w+-\w+)'
 
 
 class StudProgParser(HTMLParser):
@@ -18,7 +22,44 @@ class StudProgParser(HTMLParser):
         self.liststarted = r'Funnet 212 studieprogram' in data or (self.liststarted and not 'Studietilbud' in data)
         if self.liststarted and self.divseen and self.h3aseen:
             if 'href' in self.attrs:
-                self.result[self.key].append((data, self.attrs['href']))
+                self.result[self.key].append((data, r'http://www.uib.no' + self.attrs['href']))
             else:
                 self.key = 'P' if 'Profesjonsstudier' in data else data[0]
                 self.result[self.key] = []
+
+class StudPageParser(HTMLParser):
+    headings = {'studieplassar': '', 'poenggrense': ''}
+    key = False
+    result = {}
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if 'class' in attrs:
+            c = attrs['class']
+            if tag == 'span' and c in ['study-facts__label', 'study-facts__value']:
+                self.key = 'heading'
+            elif tag == 'td':
+                if c == 'uib-cell-indent-2':
+                    self.key = 'subject'
+                elif c == 'uib-study-data':
+                    self.key = 'uib-study-data'
+
+    def handle_data(self, data):
+        data = data.strip().lower()
+        if self.key:
+            if not self.key in self.result:
+                self.result[self.key] = []
+            if self.key == 'uib-study-data':
+                match = re.match(PATTERN_uib_study_data, data)
+                if match:
+                    data = (len(self.result['subject'])-1, data)
+                else:
+                    self.key = None
+            if self.key:
+                self.key = self.result[self.key].append(data)
+    def heading(self):
+        i = iter(self.result['heading'])
+        self.headings.update(dict(zip(i, i)))
+        return self.headings
+    def data(self,key):
+        if key in self.result:
+            return self.result[key]
