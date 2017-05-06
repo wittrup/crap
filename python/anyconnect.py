6329307e23b17498153c244bd3d75790dd8d9371
wittrup <wittrup@gmail.com>
2017-05-06 13:26:56 +0200

    anychange
    Change the scripts working directory to the script's own directory, so
    that it is able to find settings file

--- python/anyconnect.py
+++ python/anyconnect.py
@@ -1,4 +1,5 @@
-from os.path import isfile, exists
+from os.path import isfile, exists, abspath, dirname
+from os import chdir, getcwd
 from startup import EnumWindows, EnumWindowsProc, foreach_window, similar, windows
 from pywinauto import application
 from keyboard import PressKey, ReleaseKey, VK_TAB, VK_SHIFT
@@ -9,13 +10,16 @@ import pyotp
 
 ctok = {'c': 67, 'z': 90, 'j': 74, 'x': 88, 'v': 86, 'r': 82, 'p': 80, 'f': 70, 'q': 81, 'y': 89, 'k': 75, 't': 84, 'd': 68, 'h': 72, 'l': 76, 'i': 73, 'm': 77, 'a': 65, 's': 83, 'u': 85, 'g': 71, 'e': 69, 'w': 87, 'n': 78, 'b': 66, 'o': 79}
 timeout = 5
+chdir(dirname(abspath(__file__))) # Change the scripts working directory to the script's own directory
 file = 'ignore/Cisco AnyConnect.txt'
+print('looking for', file, 'in', getcwd())
 if exists(file):
     password, secret = [l.strip() for l in open(file).readlines()]
     otp = pyotp.TOTP(secret)
     del secret
 else:
-    exit('settings file (%s) not found' % file)
+    input('settings file (%s) not found' % file) # wait for user input before exit
+	exit()
 path = "%PROGRAMFILES(x86)%\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe"
 path = r'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe'
 app = application.Application()
@@ -40,7 +44,8 @@ start = time()
 while not re.match(pattern, l[0][1]):
     sleep(0.1)
     if time() > start + timeout:
-        exit('Waiting too long for %s prompt' % cac)
+        input('Waiting too long for %s prompt' % cac)
+		exit() # wait for user input before exit
     EnumWindows(EnumWindowsProc(foreach_window), 0)
     l = sorted([similar(title, cac) for title in windows], reverse=True)[:2]
 
